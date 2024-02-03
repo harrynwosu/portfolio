0ddc1b422a61e3b2e3942ade0f7025681b1be416
Harold Nwosu <harold.nwosu@gmail.com>
2024-02-03 02:02:15 -0600

    add links + footer updates

--- src/app/components/sections/Footer.jsx
+++ src/app/components/sections/Footer.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-        <div className='container p-12 flex justify-between'>
-            <span>Harold</span>
-            <p className='text-slate-600'>All Rights reserved.</p>
-        </div>
+    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span
+          className="text-4xl md:text-5xl text-white font-semibold"
+          style={{ fontFamily: "Brush Script MT, cursive" }}
+        >
+          Harold
+        </span>
+        <p className="text-slate-600">All Rights reserved.</p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/app/components/sections/IntroSection.jsx
+++ src/app/components/sections/IntroSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
@@ -38,14 +39,18 @@ const HeroSection = () => {
             cumque quisquam placeat velit unde corporis eaque.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-purple-500 to-cyan-500 hover:bg-slate-200 text-white">
-              Hire Me
-            </button>
-            <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-purple-500 to-cyan-500 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download Resume
-              </span>
-            </button>
+            <Link href="https://www.linkedin.com/in/harold-nwosu/" target="_blank">
+              <button className="px-1 py-1 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-purple-500 to-cyan-500 hover:bg-slate-800 text-white">
+                <span className="block bg-[121212] hover:bg-slate-800 rounded-full px-5 py-2">Hire Me</span>
+              </button>
+            </Link>
+            <Link href="https://drive.google.com/file/d/1_FmdQRwY91U1l650lbP2JMEXPsFlKxag/view?usp=sharing" target="_blank">
+              <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-purple-500 to-cyan-500 hover:bg-slate-800 text-white mt-3">
+                <span className="block bg-[121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                  Download Resume
+                </span>
+              </button>
+            </Link>
           </div>
         </div>
 
