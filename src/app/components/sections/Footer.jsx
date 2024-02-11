30fe8148503ea4ef3196cb3e1a53dc8585a5531a
Harold Nwosu <harold.nwosu@gmail.com>
2024-02-11 03:07:50 -0600

    fix mobile footer padding

--- src/app/components/sections/Footer.jsx
+++ src/app/components/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container px-4 md:px-12 py-12 flex justify-between">
         <span
           className="text-4xl md:text-5xl text-white font-semibold"
           style={{ fontFamily: "Brush Script MT, cursive" }}
